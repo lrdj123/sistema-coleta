0f11f303cdd7abb7fb96eac655a4da5312193eb4
lrdj123 <lucaslrdj1993@gmail.com>
2026-07-10 00:17:32 -0300

    Force port 5000 for health check

--- app.py
+++ app.py
@@ -321,5 +321,4 @@ def excluir_agendamento(id):
     return redirect(url_for('admin_agendamentos'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=5000)
