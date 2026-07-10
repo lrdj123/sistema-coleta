67379ebde3820f1e5ab815811da0daf4dd4f1f71
lrdj123 <lucaslrdj1993@gmail.com>
2026-07-09 23:22:59 -0300

    Fix port for deployment

--- app.py
+++ app.py
@@ -311,4 +311,5 @@ def api_calcular():
     return jsonify({'total': round(total, 2), 'detalhes': detalhes})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
